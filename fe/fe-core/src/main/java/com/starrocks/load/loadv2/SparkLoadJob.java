61a0c4b422520bb229e6434b6182388c74c8e489
abc982627271 <liuxuefen@starrocks.com>
2023-05-12 11:22:41 +0800

    upt
    Signed-off-by: abc982627271 <liuxuefen@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkLoadJob.java
+++ fe/fe-core/src/main/java/com/starrocks/load/loadv2/SparkLoadJob.java
@@ -85,10 +85,11 @@ import com.starrocks.metric.TableMetricsEntity;
 import com.starrocks.metric.TableMetricsRegistry;
 import com.starrocks.qe.OriginStatement;
 import com.starrocks.server.GlobalStateMgr;
+import com.starrocks.server.RunMode;
 import com.starrocks.service.FrontendOptions;
 import com.starrocks.sql.ast.LoadStmt;
 import com.starrocks.sql.ast.ResourceDesc;
-import com.starrocks.system.Backend;
+import com.starrocks.system.ComputeNode;
 import com.starrocks.task.AgentBatchTask;
 import com.starrocks.task.AgentTaskExecutor;
 import com.starrocks.task.AgentTaskQueue;
@@ -513,7 +514,7 @@ public class SparkLoadJob extends BulkLoadJob {
                                         long replicaId = replica.getId();
                                         tabletAllReplicas.add(replicaId);
                                         long backendId = replica.getBackendId();
-                                        Backend backend = GlobalStateMgr.getCurrentState().getCurrentSystemInfo()
+                                        ComputeNode backend = GlobalStateMgr.getCurrentState().getCurrentSystemInfo()
                                                 .getBackend(backendId);
 
                                         pushTask(backendId, tableId, partitionId, indexId, tabletId,
@@ -536,11 +537,17 @@ public class SparkLoadJob extends BulkLoadJob {
                                 } else {
                                     // lake tablet
                                     long backendId = ((LakeTablet) tablet).getPrimaryComputeNodeId();
-                                    Backend backend = GlobalStateMgr.getCurrentSystemInfo().
+                                    // TODO: need to refactor after be split into cn + dn
+                                    ComputeNode backend = GlobalStateMgr.getCurrentSystemInfo().
                                             getBackend(backendId);
                                     if (backend == null) {
-                                        LOG.warn("replica {} not exists", backendId);
-                                        continue;
+                                        if (RunMode.getCurrentRunMode() == RunMode.SHARED_DATA) {
+                                            backend = GlobalStateMgr.getCurrentSystemInfo().getComputeNode(backendId);
+                                        }
+                                        if (backend == null) {
+                                            LOG.warn("replica {} not exists", backendId);
+                                            continue;
+                                        }
                                     }
 
                                     pushTask(backend.getId(), tableId, partitionId, indexId, tabletId,
@@ -585,7 +592,7 @@ public class SparkLoadJob extends BulkLoadJob {
                           PushBrokerReaderParams params,
                           AgentBatchTask batchTask,
                           String tabletMetaStr,
-                          Backend backend, Replica replica, Set<Long> tabletFinishedReplicas,
+                          ComputeNode backend, Replica replica, Set<Long> tabletFinishedReplicas,
                           TTabletType tabletType)
             throws AnalysisException {
 
